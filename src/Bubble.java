c4d0e78c81d0b3e541745fc4077a5e7b8a6dde61
caojiantao <caojiantao30@gmail.com>
2018-05-02 15:41:23 +0800

    堆排序

--- src/Bubble.java
+++ src/Bubble.java
@@ -5,19 +5,20 @@ import java.util.Arrays;
  */
 public class Bubble {
 
-  public static void main(String[] args) {
-    int[] data = {10, 6, 1, 2, 4, 3, 7, 9, 2};
-    System.out.println(Arrays.toString(sortByBubble(data)));
-  }
-
-  private static int[] sortByBubble(int[] data) {
-    for (int i = 0; i < data.length - 1; i++) {
-      for (int j = 0; j < data.length - 1 - i; j++) {
-        if (data[j] > data[j + 1]) {
-          Util.exchange(data, j, j + 1);
-        }
-      }
+    public static void main(String[] args) {
+        int[] data = {6, 10, 1, 2, 4, 3, 7, 9, 2};
+        System.out.println(Arrays.toString(sortByBubble(data)));
     }
-    return data;
-  }
+
+    private static int[] sortByBubble(int[] data) {
+        int len = data.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (data[j] > data[j + 1]) {
+                    Util.exchange(data, j, j + 1);
+                }
+            }
+        }
+        return data;
+    }
 }
--- src/Fast.java
+++ src/Fast.java
@@ -5,29 +5,27 @@ import java.util.Arrays;
  */
 public class Fast {
 
-  public static void main(String[] args) {
-    int[] data = {11, 6, 2, 5, 4, 10, 9, 7, 5, 12};
-    sortByFast(data, 0, data.length - 1);
-    System.out.println(Arrays.toString(data));
-  }
+    public static void main(String[] args) {
+        int[] data = {11, 6, 2, 5, 4, 10, 9, 7, 5, 12};
+        sortByFast(data, 0, data.length - 1);
+        System.out.println(Arrays.toString(data));
+    }
 
-  /**
-   * 类似二分法，递归调用
-   */
-  private static void sortByFast(int[] data, int start, int end) {
-    int i = start, j = end;
-    int key = data[i];
-    while (i != j) {
-      while (j > i && data[j] >= key) j--;
-      Util.exchange(data, i, j);
-      while (i < j && data[i] < key) i++;
-      Util.exchange(data, i, j);
+    /**
+     * 类似二分法，递归调用
+     */
+    private static void sortByFast(int[] data, int start, int end) {
+        if (start < end) {
+            int i = start, j = end;
+            int key = data[i];
+            while (i < j) {
+                while (j > i && data[j] >= key) j--;
+                Util.exchange(data, i, j);
+                while (i < j && data[i] < key) i++;
+                Util.exchange(data, i, j);
+            }
+            sortByFast(data, start, i - 1);
+            sortByFast(data, j + 1, end);
+        }
     }
-    if (i != start) {
-      sortByFast(data, start, i - 1);
-    }
-    if (j != end) {
-      sortByFast(data, j + 1, end);
-    }
-  }
 }
